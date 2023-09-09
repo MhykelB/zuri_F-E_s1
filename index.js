fdb19392f82b109506b6a0d314592e6d95343b9c
unknown <MhykelB>
2023-09-10 00:08:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const weekDay = document.querySelector(".day");
+const utcTime = document.querySelector(".time");
+
+const weekDays = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+};
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  weekDay.textContent = weekDays[new Date().getDay()];
+  setInterval(() => {
+    utcTime.textContent = Date.now();
+  }, 1000);
+});
